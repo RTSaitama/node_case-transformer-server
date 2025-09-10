6acde1197904409baf167dae692127d9a20ea83f
RTSaitama <rtsaitama@outlook.com>
2025-09-10 13:39:43 +0300

    fixed

--- src/createServer.js
+++ src/createServer.js
@@ -4,67 +4,68 @@ const { convertToCase } = require('./convertToCase');
 
 const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+function createServer() {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
 
-  const reqUrl = new URL(req.url, `http://${req.headers.host}`);
-  const pathname = reqUrl.pathname;
-  const query = Object.fromEntries(reqUrl.searchParams);
+    const [pathPart, queryString] = req.url.split('?');
+    const text = pathPart ? pathPart.slice(1) : '';
+    const params = new URLSearchParams(queryString);
+    const toCase = params.get('toCase');
 
-  const text = pathname.slice(1);
-  const toCase = query.toCase;
+    const errors = [];
 
-  const errors = [];
+    if (!text) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
 
-  if (!text) {
-    errors.push({
-      message:
-        'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  }
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!validCases.includes(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
 
-  if (!toCase) {
-    errors.push({
-      message:
-        '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  } else if (!validCases.includes(toCase)) {
-    errors.push({
-      message:
-        'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    });
-  }
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad Request';
+      res.end(JSON.stringify({ errors }));
 
-  if (errors.length > 0) {
-    res.statusCode = 400;
-    res.end(JSON.stringify({ errors }));
+      return;
+    }
 
-    return;
-  }
+    try {
+      const result = convertToCase(text, toCase);
 
-  try {
-    const result = convertToCase(text, toCase);
+      const response = {
+        originalCase: result.originalCase,
+        targetCase: toCase,
+        convertedText: result.convertedText,
+        originalText: text,
+      };
 
-    const response = {
-      originalCase: result.originalCase,
-      targetCase: toCase,
-      convertedText: result.convertedText,
-      originalText: text,
-    };
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+      res.end(JSON.stringify(response));
+    } catch (error) {
+      res.statusCode = 500;
+      res.statusMessage = 'Internal Server Error';
 
-    res.statusCode = 200;
-    res.end(JSON.stringify(response));
-  } catch (error) {
-    res.statusCode = 500;
-
-    res.end(
-      JSON.stringify({
-        errors: [{ message: error.message }],
-      }),
-    );
-  }
-});
-
-const createServer = () => server;
+      res.end(
+        JSON.stringify({
+          errors: [{ message: error.message }],
+        }),
+      );
+    }
+  });
+}
 
 module.exports = { createServer };
